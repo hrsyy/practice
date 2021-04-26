456b1e7c28ab8e1c53ed99b45708809c57f232d3
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-26 16:38:32 +0900

    Add files via upload

---
+++ SoloLearn/pointer.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void promotion(int &megabytes)
+{
+    int multipler;
+    cin >> multipler;
+    megabytes *= multipler;
+}
+
+int main()
+{
+    int megabytes;
+    cin >> megabytes;
+
+    cout << "Before the promotion: " << megabytes << endl;
+
+    promotion(megabytes);
+    
+    cout << "After the promotion: " << megabytes << endl;
+    
+    return 0;
+}
