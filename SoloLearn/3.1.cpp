685848508263ff98c8086722617ca1aae53b24e9
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-26 09:38:46 +0900

    Add files via upload

---
+++ SoloLearn/3.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    {
+        cout << "Это " << endl;
+        cout << "очень \n";
+        cout << "круто! \n";
+
+        return 0;
+    }
+}
