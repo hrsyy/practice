811627b963ae724c9acd6ad16231358bbf55d072
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-26 16:44:36 +0900

    Add files via upload

---
+++ SoloLearn/Bird.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Bird
+{
+public:
+    void makeSound()
+    {
+        cout << "Chirp-chirp" << endl;
+    }
+};
+
+
+int main()
+{
+    Bird someBird;
+
+    someBird.makeSound();
+
+    return 0;
+}
