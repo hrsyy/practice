27e01543e8fc635ce2069c9d74a38f25940cb2c0
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-26 09:35:53 +0900

    Add files via upload

---
+++ SoloLearn/array to a function.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void winners(string custom[], const int size, int winner)
+{
+    for (int i = 0; i < size; i++)
+    {
+        i += winner - 1;
+        if (i < size)
+        {
+            cout << custom[i] << endl;
+        }
+        else 
+        {
+            break;
+        }
+    }
+}
+
+
+int main()
+{
+    string customers[] = { "Alice", "Bob", "Rayan", "Emma", "Ann",
+                           "Bruce", "Synthia", "Daniel", "Richard",
+                           "Sam", "Nick", "Mary", "Paul"};
+
+    int winner;
+    cin >> winner;
+
+    winners(customers, 13, winner);
+}
