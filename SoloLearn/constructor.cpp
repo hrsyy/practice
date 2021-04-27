8fe8f3df177721c6f2ad051451d06e6939d9fa64
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-28 08:02:03 +0900

    Add files via upload

---
+++ SoloLearn/constructor.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Painting
+{
+public:
+	Painting(string userName)
+	{
+		name = userName;
+		cout << name;
+	}
+
+
+private:
+	string name;
+};
+
+int main()
+{
+	string userName;
+	cin >> userName;
+
+	Painting Painting(userName);
+
+	return 0;
+}
