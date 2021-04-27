1d569a7fa913ff630f088bd5bf51af1f86cd48ed
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-04-27 19:44:57 +0900

    Add files via upload

---
+++ SoloLearn/Incapsulation.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class horsePowerCheck
+{
+public:
+	void setHorsepower(double userValue)
+	{
+		Horsepower = userValue;
+		if (Horsepower <= maxValue)
+		{
+			check = true;
+		}
+		else
+		{
+			check = false;
+		}
+	}
+
+
+	double getHorsepower()
+	{
+		return Horsepower;
+	}
+
+	int getmaxValue()
+	{
+		return maxValue;
+	}
+
+	void Print()
+	{
+		if (check)
+		{
+			cout << "Лошадиные силы в норме!\n" << Horsepower;
+		}
+		else
+		{
+			cout << "Слишком много!\n" << Horsepower;
+		}
+	}
+
+
+private:
+	double Horsepower;
+	int maxValue = 800;
+	bool check;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	double userValue;
+	cout << "Введите количество лошадиных сил: ";
+	cin >> userValue;
+
+	horsePowerCheck Check;
+
+	Check.setHorsepower(userValue);
+	Check.Print();
+
+	return 0;
+}
