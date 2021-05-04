fe6fbac6a3e9a658a92c9e2f2fad7534317fb74d
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-05-04 11:13:03 +0900

    Add files via upload

---
+++ SoloLearn/destructor.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+class Player
+{
+public:
+    int points;
+
+    Player(int x)
+    {
+        points = x;
+        points %= 5;
+    }
+
+    ~Player()
+    {
+        cout << points;
+    }
+};
+
+
+int main()
+{
+    int points;
+
+    cin >> points;
+
+    Player obj(points);
+}
