1249ec479b878be07bb160084e7ddc1c15b70328
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-05-08 13:05:32 +0900

    Add files via upload

---
+++ SoloLearn/Product.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+class Product
+{
+public:
+    void info()
+    {
+        cout << price << ", " << weight;
+    }
+protected:
+    double price;
+    int weight;
+};
+
+class Fruit : public Product
+{
+public:
+    string type;
+    void setInfo(double p, int w)
+    {
+        price = p;
+        weight = w;
+    }
+};
+
+int main()
+{
+    Fruit obj;
+    obj.type = "Apple";
+    obj.setInfo(4.99, 10);
+    obj.info();
+
+    return 0;
+}
