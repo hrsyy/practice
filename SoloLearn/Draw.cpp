ca5e884f63c8b995989df60dca5ad238636889f5
hrsyy <45510118+hrsyy@users.noreply.github.com>
2021-05-08 11:41:07 +0900

    Add files via upload

---
+++ SoloLearn/Draw.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Shape
+{
+public:
+    void draw()
+    {
+        cout << "Drawing...";
+    }
+};
+
+class Rectangle :public Shape
+{
+public:
+    Rectangle(int w, int h) : width(w), height(h)
+    {
+        cout << w << "x" << h << endl;
+    }
+private:
+    int width;
+    int height;
+};
+
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    Rectangle d(x, y);
+    d.draw();
+
+    return 0;
+}
